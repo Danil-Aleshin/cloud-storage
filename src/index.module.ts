98c09ff658ebbba3f64e5fdd24c52cdadbfc79ef
D.Aleshin <d.aleshyn@infomaximum.com>
2023-11-06 12:11:28 +0300

    Initial commit

---
+++ src/index.module.ts
@@ -0,0 +1,10 @@
+import { getRoutesConfig } from "./shared/configs";
+import { IModule, Module, registerModule } from "../../../libs/module-expander";
+
+export class CloudStorageModule extends Module implements IModule {}
+
+defineModule({
+  name: "",
+  routesConfig: getRoutesConfig,
+})(CloudStorageModule)
+
